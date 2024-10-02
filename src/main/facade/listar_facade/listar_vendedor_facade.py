2100a3148f0b5a513a4fc8435d49f2d1e45ea698
Vinicius Candeia <viniciuscandeia12@gmail.com>
2024-10-02 18:37:52 -0300

    ref(controller): Unificando o controller da loja

--- src/main/facade/listar_facade/listar_vendedor_facade.py
+++ src/main/facade/listar_facade/listar_vendedor_facade.py
@@ -1,37 +1,12 @@
-"""
-Módulo para o processo de listagem de vendedores.
-
-Este módulo contém a função `listar_vendedor_facade`, que coordena a listagem de vendedores
-no sistema. A função cria instâncias da view e do controller responsáveis pela
-exibição e verificação da lista de vendedores.
-
-Funções:
-- listar_vendedor_facade: Cria as instâncias necessárias para listar vendedores e exibe a
-  mensagem correspondente na interface do usuário.
-"""
-
-from ....controllers.listar_controller.listar_vendedor_controller import (
-    ListarVendedoresController,
-)
 from ....views.listar_views.listar_vendedor_view import ListarVendedoresView
+from ....controllers.vendedor_controller import VendedorController
 
 
 def listar_vendedor_facade():
-    """
-    Função construtora para listar vendedores.
-
-    Esta função cria as instâncias da view e do controller para listar os vendedores.
-    Verifica se existem vendedores cadastrados e exibe uma mensagem correspondente
-    na interface do usuário.
-
-    Fluxo:
-    - Verifica a existência de vendedores no repositório.
-    - Exibe uma mensagem indicando se a lista de vendedores está vazia ou preenchida.
-    """
     listar_vendedor_view = ListarVendedoresView()
-    listar_vendedor_controller = ListarVendedoresController()
+    vendedor_controller = VendedorController()
 
-    resposta: bool = listar_vendedor_controller.listar()
+    resposta: bool = vendedor_controller.listar()
 
     if resposta:
         listar_vendedor_view.lista_preenchida()
--- src/main/facade/loja_facade.py
+++ src/main/facade/loja_facade.py
@@ -1,80 +1,78 @@
-from src.main.facade.listar_facade.listar_facade import Listarfacade
 from src.views.listar_views.listar_lojas_view import ListarLojasView
-from src.controllers.listar_controller.listar_loja_controller import ListarLojaController
-from src.controllers.editar_controller.editar_loja_controller import EditarLojaController
 from src.models.repository.gerente_repository import GerenteRepositorio
 from src.models.repository.administrador_repository import AdministradorRepositorio
-from src.controllers.excluir_controller.excluir_loja_controller import ExcluirLojaController
-from src.controllers.adicionar_controller.adicionar_loja_controller import RegistrarLojaController
 from src.models.entities.loja_entity import Loja
 
-class Lojafacade(Listarfacade):
+from ...controllers.loja_controller import LojaController
+
+class LojaFacade:
     def __init__(self,gerenteRepository:GerenteRepositorio, administradorRepository:AdministradorRepositorio) -> None:
         super().__init__()
         self.listar_loja_view = ListarLojasView()
-        self.listar_loja_controller = ListarLojaController(administradorRepository=administradorRepository,
-                                                           gerenteRepository=gerenteRepository)
-        self.editar_loja_controller = EditarLojaController(administradorRepository=administradorRepository,
-                                                           gerenteRepository=gerenteRepository)
-        self.excluir_loja_controller = ExcluirLojaController(administradorRepository=administradorRepository,
-                                                           gerenteRepository=gerenteRepository)
-        self.registrar_loja_controller = RegistrarLojaController(administradorRepository=administradorRepository,
-                                                           gerenteRepository=gerenteRepository)
+        self.loja_controller = LojaController(administradorRepository=administradorRepository,
+                                              gerenteRepository=gerenteRepository)
 
     def registrar_loja(self, id_admnistrador:int, loja:Loja):
         try:
-            created = self.registrar_loja_controller.registrarLoja(id_administrador=id_admnistrador, loja=loja)
+            created = self.loja_controller.registrarLoja(
+                id_administrador=id_admnistrador, loja=loja)
             if created:
                 print('Loja Criada')
         except Exception as error:
             print(error)
+
     def listar_adm(self, id_adm:int):
         try:
-            list = self.listar_loja_controller.list_lojas_adm(id_adm=id_adm)
+            list = self.loja_controller.list_lojas_adm(id_adm=id_adm)
 
             self.listar_loja_view.listar(list)
         except Exception as error:
-            print(error);
+            print(error)
 
     def get_loja_adm(self, id_adm:int, id_loja:int):
          try:
-            list = self.listar_loja_controller.get_loja_adm(id_adm=id_adm, id_loja=id_loja)
+            list = self.loja_controller.get_loja_adm(
+                id_adm=id_adm, id_loja=id_loja)
 
             self.listar_loja_view.listar([list])
             return list
          except Exception as error:
-            print(error);
+            print(error)
 
     def get_loja_gerente(self, id_gerente:int, id_loja:int):
         try:
-            list = self.listar_loja_controller.get_loja_gerente(id_gerente=id_gerente, id_loja=id_loja)
+            list = self.loja_controller.get_loja_gerente(
+                id_gerente=id_gerente, id_loja=id_loja)
 
             self.listar_loja_view.listar([list])
             return list
         except Exception as error:
-            print(error);
+            print(error)
 
     def editar_loja_adm(self, id_adm:int, id_loja:int, nova_loja:dict):
         try:
-            item = self.editar_loja_controller.editar_loja_adm(id_adm=id_adm, id_loja=id_loja, nova_loja=nova_loja)
+            item = self.loja_controller.editar_loja_adm(
+                id_adm=id_adm, id_loja=id_loja, nova_loja=nova_loja)
             self.listar_loja_view.listar([item])
 
             return item
         except Exception as error:
-            print(error);
+            print(error)
 
     def editar_loja_gerente(self, id_gerente:int, id_loja:int, nova_loja:dict):
         try:
-            item = self.editar_loja_controller.editar_loja_gerente(id_gerente=id_gerente, id_loja=id_loja, nova_loja=nova_loja)
+            item = self.loja_controller.editar_loja_gerente(
+                id_gerente=id_gerente, id_loja=id_loja, nova_loja=nova_loja)
             self.listar_loja_view.listar([item])
 
             return item
         except Exception as error:
-            print(error);
+            print(error)
 
     def excluir_loja(self, id_administrador: int, id_loja: int):
         try:
-            item_excluido = self.excluir_loja_controller.excluir_loja(id_adm=id_administrador, id_loja=id_loja)
+            item_excluido = self.loja_controller.excluir_loja(
+                id_adm=id_administrador, id_loja=id_loja)
 
             self.listar_loja_view.listar([item_excluido])
 
@@ -82,6 +80,5 @@ class Lojafacade(Listarfacade):
         except Exception as error:
             print(error)
 
-
-loja_facade = Lojafacade(administradorRepository=AdministradorRepositorio(),
+loja_facade = LojaFacade(administradorRepository=AdministradorRepositorio(),
                                                 gerenteRepository=GerenteRepositorio(),)
